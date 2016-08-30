82468659f5aeb9772f6dc042174929fd1b2850e8
Oxagile Bhavesh <bhavesh.waghela@oxagile.in>
2016-08-30 20:06:38 +0530

    intial Designing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,8 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     if current_user.admin? 
       rails_admin.dashboard_path
-    end
+    else 
+      root_path
+    end	
   end
 end
